5cad3bf81bcb178db8010c7820f779cf7d99848d
Henrique Gustavo <henriquesousaa37+1@gmail.com>
2024-06-27 18:59:34 -0300

    1.01

---
+++ utils/Deposit.java
@@ -0,0 +1,19 @@
+package org.jbank.utils;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public final class Deposit extends Movement {
+
+    public Deposit(long id, @NotNull Customer customer, double amount) {
+        super(id, customer, amount);
+        executeOperation();
+    }
+
+    private void executeOperation() {
+        getCustomer().setBalance(getCustomer().getBalance() + getAmount());
+    }
+}
