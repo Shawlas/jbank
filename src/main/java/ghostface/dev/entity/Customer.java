e021dbb5b4ffe6c159c5c187af2d723484903f32
ghost_face_dev <henriquedev6@gmail.com>
2024-10-02 23:49:43 -0300

    improved system

--- src/main/java/ghostface/dev/entity/Customer.java
+++ src/main/java/ghostface/dev/entity/Customer.java
@@ -1,5 +1,20 @@
 package ghostface.dev.entity;
 
+import ghostface.dev.impl.Accounts;
+import ghostface.dev.registry.*;
+import org.jetbrains.annotations.NotNull;
+
 public interface Customer {
-    
+
+    @NotNull Name getName();
+
+    @NotNull CPF getCPF();
+
+    @NotNull Email getEmail();
+
+    @NotNull PhoneNumber getNumber();
+
+    @NotNull Birth getBirth();
+
+    @NotNull Accounts getAccounts();
 }
